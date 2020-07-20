00c2cf0257badb38879c8f6e970329f1541a618a
Dorota Potulska <dorota-chajewska@wp.pl>
2020-07-20 22:02:45 +0200

    v0.0

---
+++ src/main/java/com/sda/javagda34/webappdemo/servlets/StudentDeleteController.java
@@ -0,0 +1,36 @@
+package com.sda.javagda34.webappdemo.servlets;
+
+import com.sda.javagda34.webappdemo.database.EntityDao;
+import com.sda.javagda34.webappdemo.model.Student;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@WebServlet("/students/delete")
+public class StudentDeleteController extends HttpServlet {
+    private final EntityDao<Student> studentEntityDao = new EntityDao<>();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // localhost:8080/studentDeleteHandler.jsp?studentId=5
+        // usunięcie studenta z numerem indeksu 5
+        String studentId = req.getParameter("studentId");
+        Optional<Student> studentOptional = studentEntityDao.findById(Long.parseLong(studentId), Student.class);
+        if(studentOptional.isPresent()) {
+            Student student = studentOptional.get();
+            studentEntityDao.delete(student);
+            System.out.println("Success!");
+        }else{
+            System.out.println("Does not exist!");
+        }
+        resp.sendRedirect("/students");
+    }
+}
+
+
---
+++ src/main/java/com/sda/javagda34/webappdemo/servlets/StudentEditController.java
@@ -0,0 +1,59 @@
+package com.sda.javagda34.webappdemo.servlets;
+
+import com.sda.javagda34.webappdemo.database.EntityDao;
+import com.sda.javagda34.webappdemo.model.Gender;
+import com.sda.javagda34.webappdemo.model.Student;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@WebServlet("/students/edit")
+public class StudentEditController extends HttpServlet {
+    private final EntityDao<Student> studentEntityDao = new EntityDao<>();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String studentId = req.getParameter("studentId");
+        Optional<Student> studentOptional = studentEntityDao.findById(Long.parseLong(studentId), Student.class);
+
+        if (studentOptional.isPresent()) {
+            Student student = studentOptional.get();
+            req.setAttribute("student_to_edit", student);
+
+            // tutaj ładujemy formularz i wyświetlamy go użytkownikowi
+            req.getRequestDispatcher("/studentForm.jsp").forward(req, resp);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Long id = Long.valueOf(req.getParameter("id"));
+        String index = req.getParameter("studentIndex");
+        String firstName = req.getParameter("firstName");
+        String lastName = req.getParameter("lastName");
+        String average = req.getParameter("average");
+        String gender = req.getParameter("gender");
+        String active = req.getParameter("active");
+
+        Student student = Student.builder()
+                .id(id)
+                .indexNumber(index)
+                .firstName(firstName)
+                .lastName(lastName)
+                .average(Double.parseDouble(average))
+                .gender(Gender.valueOf(gender))
+                .active(active != null && active.equalsIgnoreCase("on"))
+                .build();
+
+        studentEntityDao.saveOrUpdate(student);
+
+        resp.sendRedirect("/students");
+    }
+}
